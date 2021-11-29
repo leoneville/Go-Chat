bfc782e64a46de7f45eabb2946f845a458c6c95f
leoneville <leoneville_@hotmail.com>
2021-11-29 09:55:55 -0300

    first commit

---
+++ go-chat_backend/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"github.com/pusher/pusher-http-go"
+
+	"log"
+	"os"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	pusherClient := pusher.Client{
+		AppID:   os.Getenv("PUSHER_APP_ID"),
+		Key:     os.Getenv("PUSHER_KEY"),
+		Secret:  os.Getenv("PUSHER_SECRET"),
+		Cluster: "us2",
+		Secure:  true,
+	}
+
+	app.Post("/api/messages", func(c *fiber.Ctx) error {
+		var data map[string]string
+
+		if err := c.BodyParser(&data); err != nil {
+			return err
+		}
+
+		pusherClient.Trigger("chat", "message", data)
+
+		return c.JSON([]string{})
+	})
+
+	app.Listen(":8000")
+}
